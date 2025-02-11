eb1231dc6f629799e24a3407a69f010c27763e6a
netherhouse <nether3ord@gmail.com>
2025-02-11 02:17:42 +0100

    добавление путей для навигации

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import History from '../pages/History/History';
-import About from '../pages/About/About';
-import Events from '../pages/Events/Events';
-import SingTogether from '../pages/SingTogether/SingTogether';
-import Repertoire from '../pages/Repertoire/Repertoire';
-import Contact from '../pages/Contact/Contact';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import { Suspense, lazy } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "components/Layout";
 
-const AppRouter: React.FC = () => {
-    return (
-        <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/history" component={History} />
-                <Route path="/about" component={About} />
-                <Route path="/events" component={Events} />
-                <Route path="/sing-together" component={SingTogether} />
-                <Route path="/repertoire" component={Repertoire} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-            <Footer />
-        </Router>
-    );
-};
+// Динамический импорт страниц
+const Home = lazy(() => import("../pages/Home/Home"));
+const History = lazy(() => import("../pages/History/History"));
+const About = lazy(() => import("../pages/About/About"));
+const Events = lazy(() => import("../pages/Events/Events"));
+const SingTogether = lazy(() => import("../pages/SingTogether/SingTogether"));
+const Repertoire = lazy(() => import("../pages/Repertoire/Repertoire"));
+const Contact = lazy(() => import("../pages/Contact/Contact"));
+
+const AppRouter = () => (
+  <Suspense fallback={<div className="loading">Загрузка...</div>}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="history" element={<History />} />
+        <Route path="about" element={<About />} />
+        <Route path="events" element={<Events />} />
+        <Route path="sing-together" element={<SingTogether />} />
+        <Route path="repertoire" element={<Repertoire />} />
+        <Route path="contacts" element={<Contact />} />
+      </Route>
+    </Routes>
+  </Suspense>
+);
 
 export default AppRouter;
